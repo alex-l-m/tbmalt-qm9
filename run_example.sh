eec775c0a81d1e691d6500a778aad43981879253
Alexander Davis <alexlovesmolecules@gmail.com>
2024-09-04 20:59:25 -0400

    Increase size of example

--- run_example.sh
+++ run_example.sh
@@ -28,9 +28,9 @@ if [ ! -f $DATABASE_PATH ]; then
 fi
 
 # Run both methods
-python ase_dftb_qm9.py 1000
-python tbmalt_run.py 1000 0 tbmalt_results.csv
-python tbmalt_run.py 1000 1 tbmalt_results_individually.csv
+python ase_dftb_qm9.py 10000
+python tbmalt_run.py 10000 0 tbmalt_results.csv
+python tbmalt_run.py 10000 1 tbmalt_results_individually.csv
 
 # Make plot comparing them
 python make_regression_table.py
